68256b52b9b092373509bbc8b6ec4e96f51d445c
bhadhrashibu <bhadhrashibu@gmail.com>
2026-02-04 15:53:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+body {
+  margin: 0;
+  font-family: 'Comic Sans MS', cursive;
+  background: linear-gradient(#ffd6e8, #ffeef6);
+  text-align: center;
+  overflow: hidden;
+}
+
+.screen {
+  display: none;
+  padding: 50px;
+}
+
+.active {
+  display: block;
+}
+
+button {
+  padding: 12px 25px;
+  font-size: 18px;
+  margin: 15px;
+  border-radius: 25px;
+  border: none;
+  background: #ff5fa2;
+  color: white;
+  cursor: pointer;
+}
+
+function playDeeper() {
+  window.open("https://www.youtube.com/watch?v=2zJb2zZJzWQ");
+}
+
+
+
